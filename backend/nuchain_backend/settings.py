e277aaf33259fdd47a96ac92f624bb9eceb64cf8
Camille <camilledubois1995@gmail.com>
2025-07-25 02:29:48 -0400

    regenerate secret key

--- backend/nuchain_backend/settings.py
+++ backend/nuchain_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^1or_u==plp5am3tu9%p38br@ur^bv7$efbw6bv!3!=+@h$@hq'
+# SECRET_KEY = 'django-insecure-^1or_u==plp5am3tu9%p38br@ur^bv7$efbw6bv!3!=+@h$@hq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
