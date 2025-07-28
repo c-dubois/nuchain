b472fcddcb9647bd121f4d9fb120249c7c19684f
Camille <camilledubois1995@gmail.com>
2025-07-28 00:14:13 -0400

    begin implementing reactor views and update requirements for rest jwt

--- backend/apps/reactors/views.py
+++ backend/apps/reactors/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Reactor
+from .serializers import ReactorSerializer, ReactorProjectionSerializer
 
-# Create your views here.
+class ReactorViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    ViewSet for listing and retrieving reactors
+    """
+    
