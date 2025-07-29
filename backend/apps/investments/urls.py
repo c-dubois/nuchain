314b38232af6c063dcbcce5e2d7b0dff4255700d
Camille <camilledubois1995@gmail.com>
2025-07-29 18:07:33 -0400

    implemnt investments urls

---
+++ backend/apps/investments/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('apps.users.urls')),
+    path('api/', include('apps.reactors.urls')),
+    path('api/', include('apps.investments.urls')),
+]
