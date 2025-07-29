1b7b7d671f4eba520a469d6e25cc9953d7523708
Camille <camilledubois1995@gmail.com>
2025-07-29 18:03:51 -0400

    implement investments admin

--- backend/apps/investments/admin.py
+++ backend/apps/investments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Investment
 
-# Register your models here.
+@admin.register(Investment)
+class InvestmentAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 
+        'reactor', 
+        'amount_invested', 
+        'created_at'
+    ]
+    list_filter = ['created_at', 'reactor']
+    search_fields = ['user__username', 'reactor__name']
+    readonly_fields = ['created_at', 'updated_at']
