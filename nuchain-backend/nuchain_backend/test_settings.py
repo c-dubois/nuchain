c2c5d4ce40b9b4e6b1d805bd0b018d7a0e3786b4
Camille <camilledubois1995@gmail.com>
2025-08-02 18:28:37 -0400

    configure settings for django test suite

---
+++ nuchain-backend/nuchain_backend/test_settings.py
@@ -0,0 +1,32 @@
+from settings import *
+
+# Override database to use SQLite for testing
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    }
+}
+
+# Disable migrations during tests
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+    
+    def __getitem__(self, item):
+        return None
+    
+MIGRATION_MODULES = DisableMigrations()
+
+# Fast password hashing for tests
+# This is not secure and should only be used in a testing environment
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# Disable security features for testing
+DEBUG = True
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
