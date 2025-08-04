88dc55159143c473e9956c310c0e2240c4af4951
Camille <camilledubois1995@gmail.com>
2025-08-04 13:00:25 -0400

    dashboard, investments, profile page placeholders

---
+++ nuchain-frontend/src/pages/Investments.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Investments: React.FC = () => {
+    return (
+        <div>
+        <h1>Invest in Reactors</h1>
+        <p>Investment functionality coming soon...</p>
+        </div>
+    );
+};
