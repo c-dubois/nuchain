f6900edb7c71922b9fb0ee43d3b08dcc518e4cc3
Camille <camilledubois1995@gmail.com>
2025-08-12 02:32:06 -0400

    update build.sh to createsuperuser on deploy

--- nuchain-backend/build.sh
+++ nuchain-backend/build.sh
@@ -6,4 +6,19 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser from env vars if not exists
+python manage.py shell << END
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+END
+
 python manage.py create_reactors
