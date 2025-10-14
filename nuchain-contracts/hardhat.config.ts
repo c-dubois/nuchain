2378bbb307d2f9d0201a9bd5e6b648de10422886
Camille <camilledubois1995@gmail.com>
2025-10-14 16:53:47 -0400

    after some troubleshooting, reconfigure hardhat for base sepolia deployment and verification

--- nuchain-contracts/hardhat.config.ts
+++ nuchain-contracts/hardhat.config.ts
@@ -1,28 +1,14 @@
 import type { HardhatUserConfig } from "hardhat/config";
-import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import hardhatKeystore from "@nomicfoundation/hardhat-keystore";
-import hardhatVerify from "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  plugins: [
-    hardhatToolboxMochaEthers,
-    hardhatKeystore,
-    hardhatVerify,
-  ],
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
   },
@@ -35,18 +21,23 @@ const config: HardhatUserConfig = {
       chainId: 84532,
     },
   },
+  verify: {
+    etherscan: {
+      apiKey: configVariable("BASESCAN_API_KEY"),
+    },
+  },
   chainDescriptors: {
     84532: {
       name: "Base Sepolia",
       blockExplorers: {
         etherscan: {
-          name: "BaseScan",
+          name: "Basescan",
           url: "https://sepolia.basescan.org",
           apiUrl: "https://api-sepolia.basescan.org/api",
         },
       },
     },
-  }
+  },
 };
 
 export default config;
